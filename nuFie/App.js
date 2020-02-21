a0c063c388d5314440b96bea427a3fb8fa3fa399
alvihrhp <rafael.hrhp@yahoo.com>
2020-02-21 20:50:26 +0700

    Create Activity Screen Done, Friend List Invite Screen Done

--- nuFie/App.js
+++ nuFie/App.js
@@ -5,6 +5,8 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import MyPost from './src/screens/myPost'
 import Home from './src/screens/home'
+import CreateActivity from './src/screens/CreateActivity';
+import FindFriends from './src/screens/FindFriends';
 
 export default function App() {
   const Tab = createBottomTabNavigator()
@@ -12,7 +14,7 @@ export default function App() {
     <Provider store={store}>
       <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="Home" component={Home}/>
+          <Tab.Screen name="Home" component={FindFriends}/>
           <Tab.Screen name="Search" component={Home}/>
           <Tab.Screen name="My Post" component={MyPost}/>
           <Tab.Screen name="Account" component={MyPost}/>
