6c9b33824f4d94591b6437879c287dd2ad92c7f6
alvihrhp <rafael.hrhp@yahoo.com>
2020-02-23 14:50:36 +0700

    resolve conflict

--- nuFie/src/store/actions/Activity.js
+++ nuFie/src/store/actions/Activity.js
@@ -1,22 +1,23 @@
 import axios from 'axios';
 
 export const createActivity = (activity) => {
-    console.log()
     return function(dispatch, state) {
         console.log(activity.data)
         axios({
             method: 'post',
-            url: 'http://172.16.15.240:3000/activities',
+            url: 'http://192.168.43.133:3000/activities',
             data: activity.data,
             headers: {
-                token: activity.token
+                token: activity.token,
+                Accept: 'application/json',
+                'Content-Type': 'multipart/form-data',
             }
         })
         .then(response => {
             console.log(response.data, '========================')
         })
         .catch(error => {
-            console.log({error}.error);
+            console.log({error});
         })
     }
 }
