d9087d2c05cbe22b65e4fe9df5ef88bcd9812a78
Bayu Oktari <bayu.oktari@gmail.com>
2020-02-26 15:15:06 +0700

    before rebase

--- nuFie/src/screens/login.js
+++ nuFie/src/screens/login.js
@@ -12,14 +12,14 @@ import { useNavigation } from "@react-navigation/native";
 import { FontAwesome } from "@expo/vector-icons";
 import { useDispatch } from "react-redux";
 import { LoginEmailPassword } from "../store/actions/user";
-import {useSelector} from 'react-redux'
+import { useSelector } from "react-redux";
 import ButtonLogin from "../components/buttonLoginRegis";
-import Loading from '../components/loading'
-import AwesomeAlert from 'react-native-awesome-alerts'
+import Loading from "../components/loading";
+import AwesomeAlert from "react-native-awesome-alerts";
 
 export default function Login() {
-  const load = useSelector(state => state.user.loading)
-  const {errorTrigger} = useSelector(state => state.other)
+  const load = useSelector(state => state.user.loading);
+  const { errorTrigger } = useSelector(state => state.other);
   const dispatch = useDispatch();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -44,60 +44,50 @@ export default function Login() {
       behavior="height"
     >
       <View style={styles.container}>
-        <ImageBackground
-          source={require("../../assets/bg-login.jpg")}
-          style={styles.bgImage}
-        >
-          <Image
-            source={require("../../assets/homelogo.png")}
-            style={{ width: 250, height: 80, margin: 40 }}
-          />
-          <View style={styles.cardLogin}>
-            <Text style={styles.titleLogin}>LOGIN</Text>
-            <View style={styles.form}>
-              <TextField label="Email" onChange={handleEmail} />
-            </View>
-            <View style={styles.form}>
-              <TextField
-                label="Password"
-                onChange={handlePassword}
-                secureTextEntry={true}
-              />
-            </View>
-            {
-              load ? <Loading/> :
-              <ButtonLogin
-                handle={handleLogin}
-                color="#0096FE"
-                textColor="#fff"
-              />
-            }
+        <View style={styles.bgtop}>
+          <ImageBackground
+            source={require("../../assets/bg-top.png")}
+            style={{ height: "100%", width: "100%" }}
+          ></ImageBackground>
+        </View>
+        <View style={styles.bgbot}>
+          <ImageBackground
+            source={require("../../assets/bg-bot.png")}
+            style={{ height: "100%", width: "100%" }}
+          ></ImageBackground>
+        </View>
+        <View style={styles.cardLogin}>
+          <Text style={styles.titleLogin}>LOGIN</Text>
+          <View style={styles.form}>
+            <TextField label="Email" onChange={handleEmail} />
           </View>
-          <View style={styles.footer}>
-            <Text style={styles.footerText}>Sign in With Social Network</Text>
-            <View style={styles.wrapperSocial}>
-              <View
-                style={[
-                  styles.socialLogo,
-                  { backgroundColor: "#4267B2", marginRight: 10 }
-                ]}
-              >
-                <FontAwesome name="facebook" size={18} color="#fff" />
-              </View>
-              <View style={[styles.socialLogo, { backgroundColor: "#DD3C2A" }]}>
-                <FontAwesome name="google" size={18} color="#fff" />
-              </View>
-            </View>
+          <View style={styles.form}>
+            <TextField
+              label="Password"
+              onChange={handlePassword}
+              secureTextEntry={true}
+            />
           </View>
-        </ImageBackground>
-        <AwesomeAlert
-          show={errorTrigger.bool}
-          showProgress={false}
-          title="Oppssss"
-          message={errorTrigger.message}
-          onDismiss={() => {dispatch({type: 'SET_ERROR', val: {bool: false, message: ''}})}}
-        />
+          {load ? (
+            <Loading />
+          ) : (
+            <ButtonLogin
+              handle={handleLogin}
+              color="#0096FE"
+              textColor="#fff"
+            />
+          )}
+        </View>
       </View>
+      <AwesomeAlert
+        show={errorTrigger.bool}
+        showProgress={false}
+        title="Oppssss"
+        message={errorTrigger.message}
+        onDismiss={() => {
+          dispatch({ type: "SET_ERROR", val: { bool: false, message: "" } });
+        }}
+      />
     </KeyboardAvoidingView>
   );
 }
@@ -106,25 +96,25 @@ const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    backgroundColor: "#fff"
   },
-  form: {
-    width: "90%",
-    marginBottom: 10
-  },
-  titleLogin: {
-    paddingHorizontal: 30,
-    fontWeight: "700",
-    fontSize: 24
-  },
-  bgImage: {
-    height: "100%",
+  bgtop: {
+    height: 350,
     width: "100%",
-    justifyContent: "center",
-    alignItems: "center"
+    position: "absolute",
+    zIndex: -3,
+    top: -120
+  },
+  bgbot: {
+    height: 350,
+    width: "100%",
+    position: "absolute",
+    zIndex: -2,
+    bottom: -120
   },
   cardLogin: {
-    width: "85%",
+    width: "90%",
     backgroundColor: "#fff",
     justifyContent: "center",
     alignItems: "center",
@@ -138,6 +128,10 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.44,
     shadowRadius: 10.32,
     elevation: 16
+  },
+  form: {
+    width: "90%",
+    marginBottom: 10
   },
   socialLogo: {
     width: 40,
@@ -156,7 +150,14 @@ const styles = StyleSheet.create({
     bottom: 20
   },
   footerText: {
-    color: "#fff",
-    marginBottom: 10
+    color: "#002970",
+    marginBottom: 10,
+    fontWeight: "700"
+  },
+  titleLogin: {
+    paddingHorizontal: 30,
+    fontWeight: "700",
+    fontSize: 24,
+    color: "#0096FE"
   }
 });
