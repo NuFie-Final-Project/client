a4d7b7c3503bc321ac916a534c87907e701e6dad
Bayu Oktari <bayu.oktari@gmail.com>
2020-02-26 12:05:13 +0700

    redesign chat room and dropdown menu

--- nuFie/src/components/ChatWrap.js
+++ nuFie/src/components/ChatWrap.js
@@ -1,48 +1,48 @@
-import React, {useEffect, useState} from 'react'
-import {View, ScrollView, StyleSheet, Dimensions} from 'react-native'
-import Chat from './chatCard'
-import db from '../../config/config_firebase'
+import React, { useEffect, useState } from "react";
+import { View, ScrollView, StyleSheet, Dimensions } from "react-native";
+import Chat from "./chatCard";
+import db from "../../config/config_firebase";
 
 export default function ChatWrap(props) {
-    const [messages, setMessages] = useState([])
-    useEffect(() => {
-        return db.firestore()
-            .collection('chat')
-            .doc(props.roomId)
-            .collection('arrMessage')
-            .onSnapshot(function(querySnapshot) {
-                // let arrMessage = []
-                const messages = querySnapshot.docs
-                    .map(doc => {
-                        return {
-                            id: doc.id,
-                            ...doc.data()
-                        }
-                    })
-                    .sort(function(a,b){
-                        return a.createdAt.seconds - b.createdAt.seconds
-                    })
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    return db
+      .firestore()
+      .collection("chat")
+      .doc(props.roomId)
+      .collection("arrMessage")
+      .onSnapshot(function(querySnapshot) {
+        // let arrMessage = []
+        const messages = querySnapshot.docs
+          .map(doc => {
+            return {
+              id: doc.id,
+              ...doc.data()
+            };
+          })
+          .sort(function(a, b) {
+            return a.createdAt.seconds - b.createdAt.seconds;
+          });
 
-                console.log(messages[0])
-                setMessages(messages)
-                console.log('update')
-            })
-    },[])
+        console.log(messages[0]);
+        setMessages(messages);
+        console.log("update");
+      });
+  }, []);
 
-    return (
-        // <ScrollView>
-            <View style={style.container}>
-                {
-                    messages.map(el => {return <Chat key={el.id} message={el}/>})
-                }  
-            </View>
-        // </ScrollView>
-    )
-    
+  return (
+    // <ScrollView>
+    <View style={style.container}>
+      {messages.map(el => {
+        return <Chat key={el.id} message={el} />;
+      })}
+    </View>
+    // </ScrollView>
+  );
 }
 
 const style = StyleSheet.create({
-    container: {
-    }   
-})
-
+  container: {
+    width: "100%"
+  }
+});
--- nuFie/src/components/chatCard.js
+++ nuFie/src/components/chatCard.js
@@ -1,49 +1,44 @@
-import React from 'react'
-import {View, Image, Text, StyleSheet} from 'react-native'
-import {useSelector} from 'react-redux'
+import React from "react";
+import { View, Image, Text, StyleSheet } from "react-native";
+import { useSelector } from "react-redux";
 
 export default function ChatCard(props) {
-    const id = useSelector(state => state.user.login)
-    const owner = () => {
-        if(props.message.owner == id){
-            return 'row-reverse'
-        } else {
-            return 'row'
-        }
+  const id = useSelector(state => state.user.login);
+  const owner = () => {
+    if (props.message.owner == id) {
+      return { flexdir: "row-reverse", bg: "#bbe1fa" };
+    } else {
+      return { flexdir: "row", bg: "#69b3e5" };
     }
-    return (
-        <View style={[style.container, {flexDirection: owner()}]}>
-            <Image 
-                source={{uri: props.message.profile}}
-                style={style.profile} 
-            />
-            <View>
-                <Text style={style.text}>
-                    {props.message.message}
-                </Text>
-            </View>
-        </View>
-    )
-    
+  };
+  return (
+    <View style={[style.container, { flexDirection: owner().flexdir }]}>
+      <Image source={{ uri: props.message.profile }} style={style.profile} />
+      <View>
+        <Text style={[style.text, { backgroundColor: owner().bg }]}>
+          {props.message.message}
+        </Text>
+      </View>
+    </View>
+  );
 }
 
-
 const style = StyleSheet.create({
-    profile: {
-        width: 50,
-        height: 50,
-        borderRadius: 30
-    },
-    container: {
-        marginBottom: 10
-    },
-    text: {
-        marginLeft: 15,
-        marginRight: 15,
-        backgroundColor: 'grey',
-        borderRadius: 10,
-        padding: 10,
-        width: 220,
-        color: 'white'
-    }
-})
+  profile: {
+    width: 43,
+    height: 43,
+    borderRadius: 45
+  },
+  container: {
+    marginBottom: 10,
+    alignItems: "center"
+  },
+  text: {
+    marginLeft: 8,
+    marginRight: 8,
+    borderRadius: 10,
+    padding: 10,
+    maxWidth: 220,
+    color: "#000"
+  }
+});
